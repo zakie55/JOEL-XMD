1a75b68611e022f24c541c2a20d18fc7da9c203c
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-01-28 10:36:44 +0300

    Create expressmenu.js

---
+++ joel/expressmenu.js
@@ -0,0 +1,58 @@
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+cmd({
+    pattern: "menu2",
+    react: "👾",
+    desc: "get cmd list",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+let menu = {
+main: '',
+download: '',
+group: '',
+owner: '',
+convert: '',
+search: ''
+};
+
+for (let i = 0; i < commands.length; i++) {
+if (commands[i].pattern && !commands[i].dontAddCommandList) {
+menu[commands[i].category] += `*┋* ${commands[i].pattern}\n`;
+ }
+}
+
+let madeMenu = ` ʜᴇʟʟᴏ ${pushname} ʜᴀᴠᴇ ᴀ ɴɪᴄᴇ ᴅᴀʏ
+
+${menu.download}
+
+
+
+${menu.main}
+
+
+${menu.group}
+
+${menu.owner}
+
+
+
+${menu.convert}
+
+
+
+${menu.search}
+
+
+`
+
+await conn.sendMessage(from,{image:{url:config.ALIVE_IMG},caption:madeMenu},{quoted:mek})
+
+}catch(e){
+console.log(e)
+reply(`${e}`)
+}
+})
