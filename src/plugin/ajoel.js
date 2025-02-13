d7c67fef55898758d2737bb8e8ede1bf265f2a1b
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-02-13 22:12:10 +0300

    Create ajoel.js

---
+++ src/plugin/ajoel.js
@@ -0,0 +1,80 @@
+import moment from 'moment-timezone';
+import fs from 'fs';
+import os from 'os';
+import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+const { generateWAMessageFromContent, proto } = pkg;
+import config from '../../config.cjs';
+
+const alive = async (m, sock) => {
+  const prefix = config.PREFIX;
+  const mode = config.MODE;
+  const pushName = m.pushName || 'User';
+
+  const cmd = m.body.startsWith(prefix)
+    ? m.body.slice(prefix.length).split(' ')[0].toLowerCase()
+    : '';
+
+  if (cmd === "menu") {
+    await m.React('⏳'); // React with a loading icon
+    // Calculate uptime
+
+    const uptimeSeconds = process.uptime();
+    const days = Math.floor(uptimeSeconds / (24 * 3600));
+    const hours = Math.floor((uptimeSeconds % (24 * 3600)) / 3600);
+    const minutes = Math.floor((uptimeSeconds % 3600) / 60);
+    const seconds = Math.floor(uptimeSeconds % 60);
+
+    
+    // Get real time
+    const realTime = moment().tz("Asia/Karachi").format("HH:mm:ss");
+    const xtime = moment.tz("Asia/Karachi").format("HH:mm:ss");
+    const xdate = moment.tz("Asia/Karachi").format("DD/MM/YYYY");
+    const time2 = moment().tz("Asia/Karachi").format("HH:mm:ss");
+let pushwish = "";
+
+if (time2 < "05:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "11:00:00") {
+  pushwish = `Good Morning 🌄`;
+} else if (time2 < "15:00:00") {
+  pushwish = `Good Afternoon 🌅`;
+} else if (time2 < "18:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else if (time2 < "19:00:00") {
+  pushwish = `Good Evening 🌃`;
+} else {
+  pushwish = `Good Night 🌌`;
+}
+
+    const aliveMessage = `hello world`;
+
+    await m.React('✅'); // React with a success icon
+
+    sock.sendMessage(
+      m.from,
+      {
+        text: aliveMessage,
+        contextInfo: {
+          isForwarded: false,
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: '@newsletter',
+            newsletterName: "Sarkar-MD",
+            serverMessageId: -1,
+          },
+          forwardingScore: 999, // Score to indicate it has been forwarded
+          externalAdReply: {
+            title: "joel",
+            body: "joel",
+            thumbnailUrl: 'https://files.catbox.moe/s1q8so.jpeg', // Add thumbnail URL if required
+            sourceUrl: 'https://atsapp.com/channel/0029VajGHyh2phHOH5zJl73P', // Add source URL if necessary
+            mediaType: 1,
+            renderLargerThumbnail: true,
+          },
+        },
+      },
+      { quoted: m }
+    );
+  }
+};
+
+export default alive;
