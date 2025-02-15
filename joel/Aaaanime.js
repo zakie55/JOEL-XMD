2bb4ae4bdd2e1392765d54237fd45fd4fb2b8ad5
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-02-15 15:36:03 +0300

    Create Aaaanime.js

---
+++ joel/Aaaanime.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import config from '../../config.cjs';
+
+const quranVideo = async (m, gss) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const validCommands = ['waifu', 'wf', 'waifupic'];
+
+  if (validCommands.includes(cmd)) {
+    const videoUrl = `https://api.waifu.pics/sfw/waifu`;
+await m.React('⏳'); // React with a loading icon
+    await gss.sendMessage(
+      m.from,
+      {
+        video: { url: videoUrl },
+        caption: `📖 *ᴊᴏᴇʟ xᴅ v⁷* 📖\n\n¢яєαтє∂ ву ℓσя∂ ʝσєℓ`,
+      },
+      { quoted: m }
+    );
+  }
+};
+
+export default quranVideo;
