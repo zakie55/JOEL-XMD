51026cab910aea0ed5a96bb102818b39d4d3f399
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-02-21 10:35:34 +0300

    Update joelpong.js

--- joelXtec/joel/joelpong.js
+++ joelXtec/joel/joelpong.js
@@ -115,7 +115,7 @@ if (time2 < "05:00:00") {
           externalAdReply: {
             title: "ᴊᴏᴇʟ xᴅ ʙᴏᴛ ᴠ ⁷",
             body: "яєѕρσηѕє ѕρєє∂ χ",
-            thumbnailUrl: '', // Add thumbnail URL if required
+            thumbnailUrl: 'https://avatars.githubusercontent.com/u/162905644?v=4', // Add thumbnail URL if required
             sourceUrl: 'https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K', // Add source URL if necessary
             mediaType: 1,
             renderLargerThumbnail: false,
