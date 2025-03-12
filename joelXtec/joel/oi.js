cb2f66b842ed6271cdef5ce49ff8ab513de9146e
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-03-12 10:49:49 +0300

    Update oi.js

--- joelXtec/joel/oi.js
+++ joelXtec/joel/oi.js
@@ -1,35 +1,46 @@
 import moment from 'moment-timezone';
 import fs from 'fs';
 import os from 'os';
-import pkg, { prepareWAMessageMedia } from '@whiskeysockets/baileys';
+import pkg from '@whiskeysockets/baileys';
 const { generateWAMessageFromContent, proto } = pkg;
 import config from '../../config.cjs';
 
-const alive = async (m, sock) => {
-  const prefix = config.PREFIX;
-  const mode = config.MODE;
-  const pushName = m.pushName || 'User';
+// Get total memory and free memory in bytes
+const totalMemoryBytes = os.totalmem();
+const freeMemoryBytes = os.freemem();
 
-  const cmd = m.body.startsWith(prefix)
-    ? m.body.slice(prefix.length).split(' ')[0].toLowerCase()
-    : '';
+// Define unit conversions
+const byteToKB = 1 / 1024;
+const byteToMB = byteToKB / 1024;
+const byteToGB = byteToMB / 1024;
 
-  if (cmd === "men") {
-    await m.React('💮'); // React with a loading icon
-    // Calculate uptime
+// Function to format bytes to a human-readable format
+function formatBytes(bytes) {
+  if (bytes >= Math.pow(1024, 3)) {
+    return (bytes * byteToGB).toFixed(2) + ' GB';
+  } else if (bytes >= Math.pow(1024, 2)) {
+    return (bytes * byteToMB).toFixed(2) + ' MB';
+  } else if (bytes >= 1024) {
+    return (bytes * byteToKB).toFixed(2) + ' KB';
+  } else {
+    return bytes.toFixed(2) + ' bytes';
+  }
+}
 
-    const uptimeSeconds = process.uptime();
-    const days = Math.floor(uptimeSeconds / (24 * 3600));
-    const hours = Math.floor((uptimeSeconds % (24 * 3600)) / 3600);
-    const minutes = Math.floor((uptimeSeconds % 3600) / 60);
-    const seconds = Math.floor(uptimeSeconds % 60);
+// Bot Process Time
+const uptime = process.uptime();
+const day = Math.floor(uptime / (24 * 3600)); // Calculate days
+const hours = Math.floor((uptime % (24 * 3600)) / 3600); // Calculate hours
+const minutes = Math.floor((uptime % 3600) / 60); // Calculate minutes
+const seconds = Math.floor(uptime % 60); // Calculate seconds
 
-    
-    // Get real time
-    const realTime = moment().tz("Tanzania/Dodoma").format("HH:mm:ss");
-    const xtime = moment.tz("Tanzania/Dodoma").format("HH:mm:ss");
-    const xdate = moment.tz("Tanzania/Dodoma").format("DD/MM/YYYY");
-    const time2 = moment().tz("Tanzania/Dodoma").format("HH:mm:ss");
+// Uptime
+const uptimeMessage = `*I am alive now since ${day}d ${hours}h ${minutes}m ${seconds}s*`;
+const runMessage = `*☀️ ${day} Day*\n*🕐 ${hours} Hour*\n*⏰ ${minutes} Minutes*\n*⏱️ ${seconds} Seconds*\n`;
+
+const xtime = moment.tz("Asia/Colombo").format("HH:mm:ss");
+const xdate = moment.tz("Asia/Colombo").format("DD/MM/YYYY");
+const time2 = moment().tz("Asia/Colombo").format("HH:mm:ss");
 let pushwish = "";
 
 if (time2 < "05:00:00") {
@@ -46,11 +57,130 @@ if (time2 < "05:00:00") {
   pushwish = `Good Night 🌌`;
 }
 
-    const aliveMessage = `𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
+const test = async (m, Matrix) => {
+  const prefix = config.PREFIX;
+  const cmd = m.body.startsWith(prefix) ? m.body.slice(prefix.length).split(' ')[0].toLowerCase() : '';
+  const mode = config.MODE === 'public' ? 'public' : 'private';
+  const pref = config.PREFIX;
+
+  const validCommands = ['list', 'help1', 'menu1'];
+
+  if (validCommands.includes(cmd)) {
+    const str = `╭━━━〔 *ᴅᴇᴍᴏɴ-sʟᴀʏᴇʀ* 〕━━━┈⊷
+┃★╭──────────────
+┃★│ Owner : *ᴛᴇᴀᴍ sʟᴀʏᴇʀ*
+┃★│ User : *${m.pushName}*
+┃★│ Baileys : *ᴍᴜʟᴛʏ-ᴅᴇᴠɪᴄᴇ*
+┃★│ Type : *ɴᴏᴅᴇ.ᴊs*
+┃★│ Mode : *${mode}*
+┃★│ Platform : *${os.platform()}*
+┃★│ Prefix : [${prefix}]
+┃★│ Version : *1.0.0*
+┃★╰──────────────
+╰━━━━━━━━━━━━━━━┈⊷ 
+> *ʜᴇʏ ${m.pushName} ${pushwish}*
+╭━❮ 𝙲𝙾𝙽𝚅𝙴𝚁𝚃𝙴𝚁 ❯━╮
+┃◈ ${prefix}𝙰𝚃𝚃𝙿
+┃◈ ${prefix}𝙰𝚃𝚃𝙿2
+┃◈ ${prefix}𝙰𝚃𝚃𝙿3
+┃◈ ${prefix}𝙴𝙱𝙸𝙽𝙰𝚁𝚈
+┃◈ ${prefix}𝙳𝙱𝙸𝙽𝙰𝚁𝚈
+┃◈ ${prefix}𝙴𝙼𝙾𝙹𝙸𝙼𝙸𝚇
+┃◈ ${prefix}𝙼𝙿3
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙰𝙸 ❯━╮
+┃◈ ${prefix}𝙰𝚒
+┃◈ ${prefix}𝙱𝚞𝚐
+┃◈ ${prefix}𝚁𝚎𝚙𝚘𝚛𝚝
+┃◈ ${prefix}𝙶𝚙𝚝
+┃◈ ${prefix}𝙳𝚊𝚕𝚕𝚎
+┃◈ ${prefix}𝚁𝚎𝚖𝚒𝚗𝚒
+┃◈ ${prefix}𝙶𝚎𝚖𝚒𝚗𝚒
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚃𝙾𝙾𝙻 ❯━╮
+┃◈ ${prefix}𝙲𝚊𝚕𝚌𝚞𝚕𝚊𝚝𝚘𝚛
+┃◈ ${prefix}𝚃𝚎𝚖𝚙𝚖𝚊𝚒𝚕
+┃◈ ${prefix}𝙲𝚑𝚎𝚌𝚔𝚖𝚊𝚒𝚕
+┃◈ ${prefix}𝚃𝚛𝚝
+┃◈ ${prefix}𝚃𝚝𝚜
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙶𝚁𝙾𝚄𝙿 ❯━╮
+┃◈ ${prefix}𝙻𝚒𝚗𝚔𝙶𝚛𝚘𝚞𝚙
+┃◈ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚐𝚌
+┃◈ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎
+┃◈ ${prefix}𝚂𝚎𝚝𝚍𝚎𝚜𝚌
+┃◈ ${prefix}𝙶𝚛𝚘𝚞𝚙
+┃◈ ${prefix}𝙶𝚌𝚜𝚎𝚝𝚝𝚒𝚗𝚐
+┃◈ ${prefix}𝚆𝚎𝚕𝚌𝚘𝚖𝚎
+┃◈ ${prefix}𝙰𝚍𝚍
+┃◈ ${prefix}𝙺𝚒𝚌𝚔
+┃◈ ${prefix}𝙷𝚒𝚍𝚎𝚃𝚊𝚐
+┃◈ ${prefix}𝚃𝚊𝚐𝚊𝚕𝚕
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝙻𝚒𝚗𝚔
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝚃𝚘𝚡𝚒𝚌
+┃◈ ${prefix}𝙿𝚛𝚘𝚖𝚘𝚝𝚎
+┃◈ ${prefix}𝙳𝚎𝚖𝚘𝚝𝚎
+┃◈ ${prefix}𝙶𝚎𝚝𝚋𝚒𝚘
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙳𝙾𝚆𝙽𝙻𝙾𝙰𝙳 ❯━╮
+┃◈ ${prefix}𝙰𝚙𝚔
+┃◈ ${prefix}𝙵𝚊𝚌𝚎𝚋𝚘𝚘𝚔
+┃◈ ${prefix}𝙼𝚎𝚍𝚒𝚊𝚏𝚒𝚛𝚎
+┃◈ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝𝚍𝚕
+┃◈ ${prefix}𝙶𝚒𝚝𝚌𝚕𝚘𝚗𝚎
+┃◈ ${prefix}𝙶𝚍𝚛𝚒𝚟𝚎
+┃◈ ${prefix}𝙸𝚗𝚜𝚝𝚊
+┃◈ ${prefix}𝚈𝚝𝚖𝚙3
+┃◈ ${prefix}𝚈𝚝𝚖𝚙4
+┃◈ ${prefix}𝙿𝚕𝚊𝚢
+┃◈ ${prefix}𝚂𝚘𝚗𝚐
+┃◈ ${prefix}𝚅𝚒𝚍𝚎𝚘
+┃◈ ${prefix}𝚈𝚝𝚖𝚙3𝚍𝚘𝚌
+┃◈ ${prefix}𝚈𝚝𝚖𝚙4𝚍𝚘𝚌
+┃◈ ${prefix}𝚃𝚒𝚔𝚝𝚘𝚔
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚂𝙴𝙰𝚁𝙲𝙷 ❯━╮
+┃◈ ${prefix}𝙿𝚕𝚊𝚢
+┃◈ ${prefix}𝚈𝚝𝚜
+┃◈ ${prefix}𝙸𝚖𝚍𝚋
+┃◈ ${prefix}𝙶𝚘𝚘𝚐𝚕𝚎
+┃◈ ${prefix}𝙶𝚒𝚖𝚊𝚐𝚎
+┃◈ ${prefix}𝙿𝚒𝚗𝚝𝚎𝚛𝚎𝚜𝚝
+┃◈ ${prefix}𝚆𝚊𝚕𝚕𝚙𝚊𝚙𝚎𝚛
+┃◈ ${prefix}𝚆𝚒𝚔𝚒𝚖𝚎𝚍𝚒𝚊
+┃◈ ${prefix}𝚈𝚝𝚜𝚎𝚊𝚛𝚌𝚑
+┃◈ ${prefix}𝚁𝚒𝚗𝚐𝚝𝚘𝚗𝚎
+┃◈ ${prefix}𝙻𝚢𝚛𝚒𝚌𝚜
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙼𝙰𝙸𝙽 ❯━╮
+┃◈ ${prefix}𝙿𝚒𝚗𝚐
+┃◈ ${prefix}𝙰𝚕𝚒𝚟𝚎
+┃◈ ${prefix}𝙾𝚠𝚗𝚎𝚛
+┃◈ ${prefix}𝙼𝚎𝚗𝚞
+┃◈ ${prefix}𝙸𝚗𝚏𝚘𝚋𝚘𝚝
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝙾𝚆𝙽𝙴𝚁 ❯━╮
+┃◈ ${prefix}𝙹𝚘𝚒𝚗
+┃◈ ${prefix}𝙻𝚎𝚊𝚟𝚎
+┃◈ ${prefix}𝙱𝚕𝚘𝚌𝚔
+┃◈ ${prefix}𝚄𝚗𝚋𝚕𝚘𝚌𝚔
+┃◈ ${prefix}𝚂𝚎𝚝𝚙𝚙𝚋𝚘𝚝
+┃◈ ${prefix}𝙰𝚗𝚝𝚒𝚌𝚊𝚕𝚕
+┃◈ ${prefix}𝚂𝚎𝚝𝚜𝚝𝚊𝚝𝚞𝚜
+┃◈ ${prefix}𝚂𝚎𝚝𝚗𝚊𝚖𝚎𝚋𝚘𝚝
+┃◈ ${prefix}𝙰𝚞𝚝𝚘𝚃𝚢𝚙𝚒𝚗𝚐
+┃◈ ${prefix}𝙰𝚕𝚠𝚊𝚢𝚜𝙾𝚗𝚕𝚒𝚗𝚎
+┃◈ ${prefix}𝙰𝚞𝚝𝚘𝚁𝚎𝚊𝚍
+┃◈ ${prefix}𝚊𝚞𝚝𝚘𝚜𝚟𝚒𝚎𝚠
+╰━━━━━━━━━━━━━━━⪼
+╭━❮ 𝚂𝚃𝙰𝙻𝙺 ❯━╮
+┃◈ ${prefix}𝚃𝚛𝚞𝚎𝚌𝚊𝚕𝚕𝚎𝚛
+┃◈ ${prefix}𝙸𝚗𝚜𝚝𝚊𝚂𝚝𝚊𝚕𝚔
+┃◈ ${prefix}𝙶𝚒𝚝𝚑𝚞𝚋𝚂𝚝𝚊𝚕𝚔
 ╰━━━━━━━━━━━━━━━⪼`;
 
     await Matrix.sendMessage(m.from, {
-      image: fs.readFileSync('owner.jpg'),
+      image: fs.readFileSync('./media/slayer.jpg'),
       caption: str,
       contextInfo: {
         mentionedJid: [m.sender],
@@ -75,239 +205,5 @@ if (time2 < "05:00:00") {
   }
 };
 
-export default alive;
-        
-
-/*нєℓℓσ
- *${pushName}* ${pushwish}
-╭───────────────━⊷
-║ ᴊᴏᴇʟ-xᴍᴅ ᴍᴀɪɴ  ᴍᴇɴᴜ
-╰───────────────━⊷
-╭───────────────━⊷
-║ *ηαмє:* *ʝσєℓ χ∂ v³ вσт*
-║ *ρяєƒιχ:*  *${prefix}*
-║ *мσ∂є:*  *${mode}*
-║ *тιмє:*  *${realTime}*
-║ *σωηєя:*  *ℓσя∂ ʝσєℓ*
-╰───────────────━⊷
-╭───────────────━⊷
-║   *ʝσєℓ χ∂ ν тняєє*
-╰───────────────━⊷
-*❑━❒ʝσєℓ χ∂ ν тняєє❑━❒*
-
----
-
-*¢σηνєятєя*
-- αттρ
-- αттρ2
-- αттρ3
-- євιηαяу
-- ∂вιηαяу
-- ємσʝιмιχ
-- мρ3
-
----
-
-*αι*
-- αι
-- вυg
-- яєρσят
-- gρт
-- ∂αℓℓє
-- яємιηι
-- gємιηι
-
----
-
-*тσσℓ*
-- ¢αℓ¢υℓαтσя
-- тємρмαιℓ
-- ¢нє¢кмαιℓ
-- тят
-- ттѕ
-
----
-
-*gяσυρ ¢σммαη∂ѕ*
-- ℓιηкgяσυρ
-- ѕєтρρg¢
-- ѕєтηαмє
-- ѕєт∂єѕ¢
-- gяσυρ
-- g¢ѕєттιηg
-- ωєℓ¢σмє
-- α∂∂
-- кι¢к
-- нι∂єтαg
-- тαgαℓℓ
-- αηтιℓιηк
-- αηтιтσχι¢
-- ρяσмσтє
-- ∂ємσтє
-- gєтвισ
-
----
-
-*∂σωηℓσα∂*
-- αρк
-- ƒα¢євσσк
-- мє∂ιαƒιяє
-- ριηтєяєѕт∂ℓ
-- gιт¢ℓσηє
-- g∂яινє
-- ιηѕтα
-- утмρ3
-- утмρ4
-- ρℓαу
-- ѕσηg
-- νι∂єσ
-- утмρ3∂σ¢
-- утмρ4∂σ¢
-- тιктσк
-
----
-
-*ρяєηιυм ¢σммαηgѕ*
-- вυgмєηυ
-- ∂σ¢вυg
-- ℓσ¢¢яαѕн
-- αмσυηтвυg <αмσυηт>
-- ρмвυg <ηυмвєя>
-- ∂єℓαувυg <ηυмвєя>
-- тяσℓℓувυg <ηυмвєя>
-- ∂σ¢υвυg <ηυмвєя>
-- υηℓιмιтє∂вυg <ηυмвєя>
-- вσмвυg <ηυмвєя>
-- ℓαgвυg <ηυмвєя>
-- g¢вυg <gяσυρℓιηк>
-- ∂єℓαуg¢вυg <gяσυρℓιηк>
-- тяσℓℓуg¢вυg <gяσυρℓιηк>
-- ℓαввυg <gяσυρℓιηк>
-- вσмg¢вυg <gяσυρℓιηк>
-- υηℓιмιтє∂g¢вυg <gяσυρℓιηк>
-- ∂σ¢υg¢вυg <gяσυρℓιηк>
-----
-
-*ѕєαя¢н*
-- ρℓαу
-- ут
-- ιм∂в
-- gσσgℓє
-- gιмαgє
-- ριηтєяєѕт
-- ωαℓℓραρєя
-- ωιкιмє∂ια
-- утѕєαя¢н
-- яιηgтσηє
-- ℓуяι¢ѕ
-
----
-
-*мαιη ¢σммαη∂ѕ*
-- ριηg
-- αℓινє
-- σωηєя
-- мєηυ
-- ιηƒσвσт
-
----
-
-*σωηєя ¢σммαη∂ѕ*
-- ʝσιη
-- ℓєανє
-- вℓσ¢к
-- υηвℓσ¢к
-- ѕєтρρвσт
-- αηтι¢αℓℓ
-- ѕєтѕтαтυѕ
-- ѕєтηαмєвσт
-- αυтσвισ
-- αυтσтуριηg
-- αℓωαуѕσηℓιηє
-- αυтσяєα∂
-- αυтσѕνιєω
-
----
-
-*ѕтαℓкєя*
-- тяυє¢αℓℓєя
-- ιηѕтαѕтαℓк
-- gιтнυвѕтαℓк
----
-
-*σтнєя ¢σммαη∂ѕ*
-- ѕαρк
-- αρρ
-- αρρѕєαя¢н
-- ρℓαуѕтσяє
-- qυяαηνι∂єσ
-- тσυяℓ
-- υяℓ
-- ¢нαηηєℓ
-- ѕυρρσят
-- ʝσєℓ
-- ¢нαт
-- qνι∂
-- qυяαηνι∂
-- ѕѕ
-
----
-
-*ηєω ¢σммαη∂ѕ*
-- ѕ¢σяє
-- ¢ℓι¢к
-- яєѕυℓтѕ
-- gιт¢ℓσηє
-- υρ∂αтє
-- ѕηα¢кνι∂єσ
-- ѕηα¢к
-- ѕнσятєηυяℓ
-- кι¢кαℓℓ
-- ιρѕтαℓк
-- gινєтєχт
-- мє∂ιαƒιяє
-- ƒαη¢у
-- αηтι¢αℓℓ
-- ωα¢нαηηєℓ
-- мσνιє
-----
-*❑━❒ℓσя∂ ʝσєℓтє¢н❑━❑*`;
-
-    await m.React('☄️'); // React with a success icon
-
-    sock.sendMessage(
-      m.from,
-      {
-        text: aliveMessage,
-        contextInfo: {
-          isForwarded: false,
-          forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363317462952356@newsletter',
-            newsletterName: "ᴊᴏᴇʟ xᴅ ʙᴏᴛ ᴠ ⁷",
-            serverMessageId: -1,
-          },
-          forwardingScore: 999, // Score to indicate it has been forwarded
-          externalAdReply: {
-            title: "ᴊᴏᴇʟ xᴅ ʙᴏᴛ ᴠ ⁷",
-            body: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ ᴊᴏᴇʟ",
-            thumbnailUrl: 'https://avatars.githubusercontent.com/u/162905644?v=4', // Add thumbnail URL if required
-            sourceUrl: 'https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K', // Add source URL if necessary
-            mediaType: 1,
-            renderLargerThumbnail: true,
-          },
-        },
-      },
-      { quoted: m }
-    );
-    // preview menu image 
-     await Matrix.sendMessage(m.from, {
-        image: { url: `https://i.ibb.co/WcwzzY2/shaban-sobx-md.jpg` }, // 🔥 Image URL
-        caption: status
-      }, { quoted: m }
-  );
-  }
-};
-
-export default alive;
-        
-*/
+export default test;
+      
