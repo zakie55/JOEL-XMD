2dcd9d68e419462936b237db5bb7230d6500855e
joel james <162905644+joeljamestech2@users.noreply.github.com>
2025-03-20 18:13:30 +0300

    Update freebotmp3.js

--- joelXtec/joelXbot/freebotmp3.js
+++ joelXtec/joelXbot/freebotmp3.js
@@ -70,7 +70,7 @@ if (time2 < "05:00:00") {
             forwardingScore: 999, // Score to indicate it has been forwarded
             externalAdReply: {
             title: "ᴘᴏᴡᴇʀᴇᴅ ʙʏ ʟᴏʀᴅ ᴊᴏᴇʟ",
-            body: `UPTIME ${days}D ${hours}H ${minutes}M ${seconds}S`,
+            body: `hello ${pushName} your request has been sent`,
             thumbnailUrl: 'https://raw.githubusercontent.com/joeljamestech2/JOEL-XMD/refs/heads/main/mydata/media/joelXbot.jpg', // Add thumbnail URL if required
             sourceUrl: 'https://whatsapp.com/channel/0029Vak2PevK0IBh2pKJPp2K', // Add source URL if necessary
             mediaType: 1,
