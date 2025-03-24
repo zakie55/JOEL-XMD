5d6cde6c4c72a9178e7b0f9b30b4fc9c3d42c8e7
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-03-24 12:59:09 +0300

    ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ

--- joelXtec/joelXbot/hentai.js
+++ joelXtec/joelXbot/hentai.js
@@ -1,3 +1,6 @@
+
+
+
 import axios from 'axios';
 import config from '../../config.cjs';
 
@@ -7,8 +10,17 @@ const nudeCommands = async (m, gss) => {
   const validCommands = ['nude', 'nudeimage', 'nudeimg'];
 
   if (validCommands.includes(cmd)) {
-    const nudeUrl = `https://pikabotzapi.vercel.app/anime-nsfw/hentai-images/?apikey=anya-md&category=nude`;
-await m.React('⏳'); // React with a loading icon
+     // URL de l'API pour obtenir l'image de la catégorie "kissing_while_penetrated"
+        const apiUrl = 'https://pikabotzapi.vercel.app/anime-nsfw/hentai-images/?apikey=anya-md&category=kissing_while_penetrated';
+
+        // Faire une requête à l'API
+        const response = await fetch(apiUrl);
+        const data = await response.json();
+
+        // Vérification des données reçues
+        if (data && data.image) {
+            const nudeUrl = data.image; // URL de l'image reçue depuis l'API
+    await m.React('⏳'); // React with a loading icon
     await gss.sendMessage(
       m.from,
       {
@@ -21,3 +33,4 @@ await m.React('⏳'); // React with a loading icon
 };
 
 export default nudeCommands;
+  
