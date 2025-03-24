fc5ef1d26ccdced821d99cbe46412fa2777080f0
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-03-24 21:00:57 +0300

    ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ

--- config.cjs
+++ config.cjs
@@ -39,6 +39,7 @@ const config = {
   REJECT_CALL: process.env.REJECT_CALL !== undefined ? process.env.REJECT_CALL === 'true' : false, 
   NOT_ALLOW: process.env.NOT_ALLOW !== undefined ? process.env.NOT_ALLOW === 'true' : true,
   MODE: process.env.MODE || "public",
+  DELETED_MESSAGES_CHAT_ID: process.env.DELETED_MESSAGES_CHAT_ID || "255781144539@s.whatsapp.net",
   OWNER_NAME: process.env.OWNER_NAME || "ʟᴏʀᴅ ᴊᴏᴇʟ",
   OWNER_NUMBER: process.env.OWNER_NUMBER || "255714595078",
   SUDO_NUMBER: process.env.SUDO_NUMBER || "255781144539",
