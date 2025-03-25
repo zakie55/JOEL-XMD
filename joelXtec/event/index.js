6dbddd09ac83cefa2561d6574ee2350e9809b5a9
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-03-25 19:47:40 +0300

    ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ

--- joelXtec/event/index.js
+++ joelXtec/event/index.js
@@ -1,6 +1,6 @@
 import Handler from './handler.js'
 import Callupdate from './call-handler.js'
 import GroupUpdate from './group-handler.js'
-import autoStatus from './joelXtec/joelXbot/statuslike.js'
+import AutoStatus from './joelXtec/joelXbot/statuslike.js'
 
 export { Handler, Callupdate, GroupUpdate };
