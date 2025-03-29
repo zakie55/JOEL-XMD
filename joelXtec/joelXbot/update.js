fdaba594664660546a00f9ceecbd91a6b8536be2
~joeljamestech <162905644+joeljamestech2@users.noreply.github.com>
2025-03-29 12:46:23 +0300

    ᴊᴏᴇʟ xᴍᴅ ʙᴏᴛ

--- joelXtec/joelXbot/update.js
+++ joelXtec/joelXbot/update.js
@@ -86,7 +86,7 @@ const update = async (m, Matrix) => {
             await editMessage("```🔄 Replacing files...```");
 
             // Replace files while skipping important configs
-            const sourcePath = path.join(extractPath, "Whatsapp-main");
+            const sourcePath = path.join(extractPath, "JOEL-XMD-main");
             await copyFolderSync(sourcePath, process.cwd(), ['package.json', 'config.cjs', '.env']);
 
             // Update package.json with new commit hash
